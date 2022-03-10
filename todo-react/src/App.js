70f0e59c481301a08ca5b2db0cbcb4bcb2e052f3
EsotericZ <cjsand03@gmail.com>
2022-03-10 07:44:41 -0700

    second 24

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,10 +1,23 @@
 import Counter from './Counter';
+import List from './List';
 
 const App = () => { 
+  const myFaveFoods = ['Oatmeal', 'Chicken', 'Steak',];
+  const myFaveCryptos = ['XRP', 'ALGO', 'XLM',];
+  const msg1 = 'My Favorite Foods'
+  const msg2 = 'My Favorite Cryptos'
+
   return(
     <div>
       <Counter/>
-      <Counter/>
+      <List
+        message={msg1}
+        items={myFaveFoods}
+      />
+      <List
+        message={msg2}
+        items={myFaveCryptos}
+      />
     </div>
   );
 };
